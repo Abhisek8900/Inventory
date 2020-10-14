557c334484562cb584f53ed0e76add8d374f78c3
Abhishek Nayak <abhiseknayak65@gmail.com>
2020-10-14 22:45:30 +0530

    added validations and image

--- src/app/mylistitem/additem/additem.component.ts
+++ src/app/mylistitem/additem/additem.component.ts
@@ -15,6 +15,11 @@ export class AdditemComponent implements OnInit {
   description: string;
   type: string;
   id;
+  errormsg: boolean = false;
+  namemsg: boolean = false;
+  pricemsg: boolean = false;
+  error:boolean=false;
+  filesize: number;
 
   constructor(private router: Router, private route: ActivatedRoute,) { }
 
@@ -37,55 +42,70 @@ export class AdditemComponent implements OnInit {
 
   fileEvent(fileInput: HTMLInputEvent) {
     var file = fileInput.target.files[0];
-    var reader = new FileReader()
-    reader.onload = function (base64) {
-      localStorage["file"] = JSON.stringify(base64.target.result);
-      //localStorage["file"] = JSON.stringify(base64);
+    var reader = new FileReader();
+    this.filesize = file.size;
+    if (this.filesize <= 512000) {
+      this.errormsg = false;
+      reader.onload = function (base64) {
+        localStorage["file"] = JSON.stringify(base64.target.result);
+
+      }
+      this.filename = JSON.parse(localStorage["file"]);
+      reader.readAsDataURL(file);
     }
-    reader.readAsDataURL(file);
+    else {
+      this.errormsg = true;
+    }
+
+
+
   }
 
   submititem() {
-    if (this.type == "details") {
-      this.router.navigate(["/mylistitem"]);
-    }
-    else {
-      this.filename = JSON.parse(localStorage["file"]);
-      let itemid = JSON.parse(localStorage.getItem("Item"));
-      let id = 0;
-      if (itemid != null && itemid.length) {
-        id = itemid[itemid.length - 1].id + 1;
+    if (this.name && this.price && this.filename) {
+      if (this.type == "details") {
+        this.router.navigate(["/mylistitem"]);
       }
-      this.additem.push({
-        id: id,
-        name: this.name,
-        price: this.price,
-        description: this.description,
-        image: this.filename
-      });
-      if (this.additem.length) {
-        if (localStorage.getItem("Item")) {
-          let item = JSON.parse(localStorage.getItem("Item"));
-          if (item.length) {
-            this.additem.forEach(data => {
-              item.push(data);
-            });
-            localStorage.setItem("Item", JSON.stringify(item));
-            this.router.navigate(["/mylistitem"]);
+      else {
+        let itemid = JSON.parse(localStorage.getItem("Item"));
+        let id = 0;
+        if (itemid != null && itemid.length) {
+          id = itemid[itemid.length - 1].id + 1;
+        }
+        this.additem.push({
+          id: id,
+          name: this.name,
+          price: this.price,
+          description: this.description,
+          image: this.filename
+        });
+        if (this.additem.length) {
+          if (localStorage.getItem("Item")) {
+            let item = JSON.parse(localStorage.getItem("Item"));
+            if (item.length) {
+              this.additem.forEach(data => {
+                item.push(data);
+              });
+              localStorage.setItem("Item", JSON.stringify(item));
+              this.router.navigate(["/mylistitem"]);
+            }
+            else {
+              localStorage.setItem("Item", JSON.stringify(this.additem));
+              this.router.navigate(["/mylistitem"]);
+            }
           }
           else {
             localStorage.setItem("Item", JSON.stringify(this.additem));
             this.router.navigate(["/mylistitem"]);
           }
         }
-        else {
-          localStorage.setItem("Item", JSON.stringify(this.additem));
-          this.router.navigate(["/mylistitem"]);
-        }
       }
     }
 
-
-
+    else {
+      this.namemsg = this.name ? false : true;
+      this.pricemsg = this.price ? false : true;
+      this.error = this.filename ? false : true;
+    }
   }
 }
